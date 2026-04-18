66cb0c05ddc7ce1fe00075350507cbd41d59714f
Konfermat <semyon.obuhov@gmail.com>
2026-04-18 20:23:29 +0700

    first commit

---
+++ shop/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def text_math(self):
+        self.assertEqual(2+2, 4)
+
+def test_homepage(self):
+    response = self.client.get('/')
+    self.assertIn(response.status_code, [200, 404])
+    
